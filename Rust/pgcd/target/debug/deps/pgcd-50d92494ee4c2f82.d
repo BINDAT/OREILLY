bc05e4bfa4cf05512e4060e85de467d9b044989e
bindat <maximerigaud66@gmail.com>
2026-03-12 17:23:06 +0100

    Add initial implementation of GCD testing in Rust
    - Created a new Rust package `test_gcd` with version 0.1.0.
    - Added `Cargo.toml` and `Cargo.lock` files for package management.
    - Implemented a test function `test_gcd` in `src/main.rs` to validate the GCD function with various cases.
    - Generated necessary target files for build and incremental compilation.

---
+++ Rust/pgcd/target/debug/deps/pgcd-50d92494ee4c2f82.d
@@ -0,0 +1,5 @@
+/home/maxime/Documents/Github/OREILLY/Rust/pgcd/target/debug/deps/pgcd-50d92494ee4c2f82.d: src/main.rs
+
+/home/maxime/Documents/Github/OREILLY/Rust/pgcd/target/debug/deps/pgcd-50d92494ee4c2f82: src/main.rs
+
+src/main.rs:
