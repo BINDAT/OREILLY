bc9a89cc78359b54b616ed4a1e82921abed8280f
bindat <maximerigaud66@gmail.com>
2026-03-12 21:27:22 +0100

    Suppression des fichiers et répertoires obsolètes, ajout de nouveaux fichiers pour le projet exercices_rust, y compris les implémentations de gcd, pgcd, et les tests associés.

--- Rust/gcd/target/debug/deps/gcd-dc4792f0c2cfdf3f.d
+++
@@ -1,5 +0,0 @@
-/home/maxime/Documents/Github/OREILLY/Rust/gcd/target/debug/deps/gcd-dc4792f0c2cfdf3f.d: src/main.rs
-
-/home/maxime/Documents/Github/OREILLY/Rust/gcd/target/debug/deps/gcd-dc4792f0c2cfdf3f: src/main.rs
-
-src/main.rs:
--- Rust/test_gcd/target/debug/deps/test_gcd-e252e8fdded813ac.d
+++
@@ -1,5 +0,0 @@
-/home/maxime/Documents/Github/OREILLY/Rust/test_gcd/target/debug/deps/test_gcd-e252e8fdded813ac.d: src/main.rs
-
-/home/maxime/Documents/Github/OREILLY/Rust/test_gcd/target/debug/deps/test_gcd-e252e8fdded813ac: src/main.rs
-
-src/main.rs:
